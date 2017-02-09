a020f101e0ff7dee3b480469efca91cf36a9615e
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-02-09 23:26:05 +0600

    Add login support with tests
    Signed-off-by: Meraj <meraj.enigma@gmai.com>

---
+++ sample_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      if !user
+        flash.now[:danger] = 'Invalid email'
+      else
+        flash.now[:danger] = 'Invalid password'
+      end
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+end
--- sample_app/lib/delta_formatter.rb
+++ sample_app/lib/delta_formatter.rb
@@ -1,3 +1,5 @@
+require 'Logger'
+
 class DeltaFormatter < Logger::Formatter
     def call(severity, time, program_name, msg)
         "[#{severity}] #{String === msg ? msg : msg.inspect}\n"
