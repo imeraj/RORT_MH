e04425e9c2999ba240434337d5c76acfb1125872
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-02-19 13:57:02 +0600

    Add delete user support (not working fully)
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate, :only => ["show", "edit", "update", "index"]
   before_action :correct_user, :only => ["edit", "update"]
+  before_action :admin_user, :only => ["destroy"]
 
   def new
     @user = User.new
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
 	  end
   end
 
+  def destroy
+	  User.find(params[:id]).destroy
+	  flash[:success] = "User deleted"
+ 	  redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -50,4 +57,8 @@ class UsersController < ApplicationController
 	  @user = User.find(params[:id])
 	  redirect_to(root_url) unless current_user?(@user)
   end
+
+  def admin_user
+	  redirect_to(root_url) unless current_user.admin?
+  end	
 end
