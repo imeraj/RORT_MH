83e6e47a6ead1dd0323c431c0a7295ee72cb4acc
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-23 09:15:42 +0600

    Rename sample app to minitwitter
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     # Return a full title on a per-page basis
     def full_title(page_title='')
-        base_title = 'Ruby on Rails Tutorial Sample App'
+        base_title = 'Ruby on Rails Tutorial miniTwitter App'
         if page_title.empty?
             base_title
         else
